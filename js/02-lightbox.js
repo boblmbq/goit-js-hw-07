b46c0343f3dff42c07277f6d27d21df276dfa90c
boblmbq <sichkardanylo@gmail.com>
2023-07-05 18:09:44 +0200

    second task done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const ulEl = document.querySelector(".gallery");
 
-console.log(galleryItems);
+//? making markup in ul
+const ulMarkup = galleryItems
+  .map((e) => {
+    return `<li class="gallery__item">
+             <a class="gallery__link" href="${e.original}">
+                <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+            </a>
+          </li>`;
+  })
+  .join(" ");
+ulEl.insertAdjacentHTML("beforeend", ulMarkup);
+
+//? disabling default behavior
+ulEl
+  .querySelectorAll(".gallery__link")
+  .forEach((a) => a.addEventListener("click", (e) => e.preventDefault()));
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250
+});
+
+
